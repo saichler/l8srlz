533835865dc2be0d69cccb7755c82cdb1ee774bb
Sharon Aicler <saichler@gmail.com>
2025-03-26 19:41:21 -0700

    Alter Response

--- go/go.mod
+++ go/go.mod
@@ -3,15 +3,15 @@ module github.com/saichler/serializer/go
 go 1.24.1
 
 require (
-	github.com/saichler/l8test v0.0.0-20250325003525-28895eb7e99a
+	github.com/saichler/l8test/go v0.0.0-20250327011209-2f6c71c19b88
 	github.com/saichler/shared/go v0.0.0-20250325020725-e996ba087e15
-	github.com/saichler/types/go v0.0.0-20250326234517-d70453468c67
+	github.com/saichler/types/go v0.0.0-20250327023858-5ddcd8fd87f1
 	google.golang.org/protobuf v1.36.6
 )
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/saichler/layer8/go v0.0.0-20250326140701-4b8fd41ad85c // indirect
+	github.com/saichler/layer8/go v0.0.0-20250327011116-9f76b210d1a4 // indirect
 	github.com/saichler/reflect/go v0.0.0-20250326150136-4992ad9ca4ec // indirect
-	github.com/saichler/servicepoints/go v0.0.0-20250326141317-76cab4f84fac // indirect
+	github.com/saichler/servicepoints/go v0.0.0-20250327004119-6a8f849f5230 // indirect
 )
--- go/serialize/response/Response.go
+++ go/serialize/response/Response.go
@@ -5,41 +5,61 @@ import (
 	"github.com/saichler/serializer/go/serialize/object"
 	"github.com/saichler/types/go/common"
 	"github.com/saichler/types/go/types"
-	"google.golang.org/protobuf/proto"
 )
 
 type Response struct {
-	list []interface{}
-	err  error
+	elems []interface{}
+	err   error
 }
 
-func (this *Response) AddProto(pb proto.Message) {
-	if this.list == nil {
-		this.list = make([]interface{}, 0)
+func NewError(errMessage string) *Response {
+	return &Response{err: errors.New(errMessage)}
+}
+
+func New(err error, elems ...interface{}) *Response {
+	return &Response{err: err, elems: elems}
+}
+
+func (this *Response) Add(elem interface{}) {
+	if this.elems == nil {
+		this.elems = make([]interface{}, 0)
 	}
-	this.list = append(this.list, pb)
+	this.elems = append(this.elems, elem)
 }
 
 func (this *Response) SetError(err error) {
 	this.err = err
 }
 
-func (this *Response) List() []interface{} {
-	return this.list
+func (this *Response) Elems() []interface{} {
+	return this.elems
 }
 
-func (this *Response) Error() error {
+func (this *Response) Elem() interface{} {
+	if this == nil {
+		return nil
+	}
+	if this.elems == nil || len(this.elems) == 0 {
+		return nil
+	}
+	return this.elems[0]
+}
+
+func (this *Response) Err() error {
+	if this == nil {
+		return nil
+	}
 	return this.err
 }
 
 func (this *Response) ToProto() *types.Response {
 	response := &types.Response{}
-	if this.list != nil {
-		response.List = make([][]byte, len(this.list))
-		for i, pb := range this.list {
+	if this.elems != nil {
+		response.Elems = make([][]byte, len(this.elems))
+		for i, elem := range this.elems {
 			obj := object.NewEncode([]byte{}, 0)
-			obj.Add(pb)
-			response.List[i] = obj.Data()
+			obj.Add(elem)
+			response.Elems[i] = obj.Data()
 		}
 	}
 	if this.err != nil {
@@ -50,44 +70,16 @@ func (this *Response) ToProto() *types.Response {
 
 func FromProto(response *types.Response, resourcs common.IResources) *Response {
 	resp := &Response{}
-	if response.List != nil {
-		resp.list = make([]interface{}, len(response.List))
-		for i, data := range response.List {
+	if response.Elems != nil {
+		resp.elems = make([]interface{}, len(response.Elems))
+		for i, data := range response.Elems {
 			obj := object.NewDecode(data, 0, "", resourcs.Registry())
 			pb, _ := obj.Get()
-			resp.list[i] = pb
+			resp.elems[i] = pb
 		}
 	}
 	if response.ErrMsg != "" {
 		resp.err = errors.New(response.ErrMsg)
 	}
 	return resp
-}
-
-func NewMl(pbs []interface{}) *Response {
-	resp := &Response{}
-	if pbs != nil {
-		resp.list = make([]interface{}, len(pbs))
-		for i, pb := range pbs {
-			resp.list[i] = pb
-		}
-	}
-	return resp
-}
-
-func NewSl(pb interface{}) *Response {
-	resp := &Response{}
-	if pb != nil {
-		resp.list = make([]interface{}, 1)
-		resp.list[0] = pb
-	}
-	return resp
-}
-
-func NewErr(errMessage string) *Response {
-	return &Response{err: errors.New(errMessage)}
-}
-
-func NewSWE(pb interface{}, err error) *Response {
-	return &Response{err: err, list: []interface{}{pb}}
 }
