c5991ff84c4d8438845826d44f1f20cb0b834782
Sharon Aicler <saichler@gmail.com>
2025-08-09 11:13:00 -0700

    Fix small bug

--- go/serialize/object/UInt32.go
+++ go/serialize/object/UInt32.go
@@ -1,24 +1,37 @@
 package object
 
+import "encoding/binary"
+
 type UInt32 struct{}
 
 func (this *UInt32) add(any interface{}, data *[]byte, location *int) {
 	checkAndEnlarge(data, location, 4)
 	i := any.(uint32)
-	(*data)[*location+3] = byte(i)
-	(*data)[*location+2] = byte(i >> 8)
-	(*data)[*location+1] = byte(i >> 16)
-	(*data)[*location] = byte(i >> 24)
+	binary.BigEndian.PutUint32((*data)[*location:], i)
 	*location += 4
+	/*
+		checkAndEnlarge(data, location, 4)
+		i := any.(uint32)
+		(*data)[*location+3] = byte(i)
+		(*data)[*location+2] = byte(i >> 8)
+		(*data)[*location+1] = byte(i >> 16)
+		(*data)[*location] = byte(i >> 24)
+		*location += 4
+	*/
 }
 
 func (this *UInt32) get(data *[]byte, location *int) interface{} {
-	var result uint32
-	v1 := uint32((*data)[*location]) << 24
-	v2 := uint32((*data)[*location+1]) << 16
-	v3 := uint32((*data)[*location+2]) << 8
-	v4 := uint32((*data)[*location+3])
-	result = v1 + v2 + v3 + v4
+	result := binary.BigEndian.Uint32((*data)[*location:])
 	*location += 4
 	return result
+	/*
+		var result uint32
+		v1 := uint32((*data)[*location]) << 24
+		v2 := uint32((*data)[*location+1]) << 16
+		v3 := uint32((*data)[*location+2]) << 8
+		v4 := uint32((*data)[*location+3])
+		result = v1 + v2 + v3 + v4
+		*location += 4
+		return result
+	*/
 }
--- go/serialize/object/UInt64.go
+++ go/serialize/object/UInt64.go
@@ -1,31 +1,44 @@
 package object
 
+import "encoding/binary"
+
 type UInt64 struct{}
 
 func (this *UInt64) add(any interface{}, data *[]byte, location *int) {
 	checkAndEnlarge(data, location, 8)
 	i := any.(uint64)
-	(*data)[*location] = byte((i >> 56) & 0xff)
-	(*data)[*location+1] = byte((i >> 48) & 0xff)
-	(*data)[*location+2] = byte((i >> 40) & 0xff)
-	(*data)[*location+3] = byte((i >> 32) & 0xff)
-	(*data)[*location+4] = byte((i >> 24) & 0xff)
-	(*data)[*location+5] = byte((i >> 16) & 0xff)
-	(*data)[*location+6] = byte((i >> 8) & 0xff)
-	(*data)[*location+7] = byte((i) & 0xff)
+	binary.BigEndian.PutUint64((*data)[*location:], i)
 	*location += 8
+	/*
+		checkAndEnlarge(data, location, 8)
+		i := any.(uint64)
+		(*data)[*location] = byte((i >> 56) & 0xff)
+		(*data)[*location+1] = byte((i >> 48) & 0xff)
+		(*data)[*location+2] = byte((i >> 40) & 0xff)
+		(*data)[*location+3] = byte((i >> 32) & 0xff)
+		(*data)[*location+4] = byte((i >> 24) & 0xff)
+		(*data)[*location+5] = byte((i >> 16) & 0xff)
+		(*data)[*location+6] = byte((i >> 8) & 0xff)
+		(*data)[*location+7] = byte((i) & 0xff)
+		*location += 8*/
 }
 
 func (this *UInt64) get(data *[]byte, location *int) interface{} {
-	var result uint64
-	result = uint64(0xff&(*data)[*location])<<56 |
-		uint64(0xff&(*data)[*location+1])<<48 |
-		uint64(0xff&(*data)[*location+2])<<40 |
-		uint64(0xff&(*data)[*location+3])<<32 |
-		uint64(0xff&(*data)[*location+4])<<24 |
-		uint64(0xff&(*data)[*location+5])<<16 |
-		uint64(0xff&(*data)[*location+6])<<8 |
-		uint64(0xff&(*data)[*location+7])
+	result := binary.BigEndian.Uint64((*data)[*location:])
 	*location += 8
 	return result
+	/*
+		var result uint64
+		result = uint64(0xff&(*data)[*location])<<56 |
+			uint64(0xff&(*data)[*location+1])<<48 |
+			uint64(0xff&(*data)[*location+2])<<40 |
+			uint64(0xff&(*data)[*location+3])<<32 |
+			uint64(0xff&(*data)[*location+4])<<24 |
+			uint64(0xff&(*data)[*location+5])<<16 |
+			uint64(0xff&(*data)[*location+6])<<8 |
+			uint64(0xff&(*data)[*location+7])
+		*location += 8
+		return result
+
+	*/
 }
