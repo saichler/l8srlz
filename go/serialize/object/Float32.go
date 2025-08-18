630ccfb5fc2aaa3deadbd0d1d845d769d7b35935
Sharon Aicler <saichler@gmail.com>
2025-08-18 10:50:09 -0700

    Increase performance

--- go/serialize/object/Float32.go
+++ go/serialize/object/Float32.go
@@ -1,6 +1,7 @@
 package object
 
 import (
+	"encoding/binary"
 	"math"
 )
 
@@ -10,20 +11,14 @@ func (this *Float32) add(any interface{}, data *[]byte, location *int) {
 	checkAndEnlarge(data, location, 4)
 	f := any.(float32)
 	i := math.Float32bits(f)
-	(*data)[*location+3] = byte(i)
-	(*data)[*location+2] = byte(i >> 8)
-	(*data)[*location+1] = byte(i >> 16)
-	(*data)[*location] = byte(i >> 24)
+	loc := *location
+	binary.BigEndian.PutUint32((*data)[loc:loc+4], i)
 	*location += 4
 }
 
 func (this *Float32) get(data *[]byte, location *int) interface{} {
-	var result uint32
-	v1 := uint32((*data)[*location]) << 24
-	v2 := uint32((*data)[*location+1]) << 16
-	v3 := uint32((*data)[*location+2]) << 8
-	v4 := uint32((*data)[*location+3])
-	result = v1 + v2 + v3 + v4
+	loc := *location
+	result := binary.BigEndian.Uint32((*data)[loc : loc+4])
 	*location += 4
 	return math.Float32frombits(result)
 }
--- go/serialize/object/Float64.go
+++ go/serialize/object/Float64.go
@@ -1,6 +1,7 @@
 package object
 
 import (
+	"encoding/binary"
 	"math"
 )
 
@@ -10,27 +11,14 @@ func (this *Float64) add(any interface{}, data *[]byte, location *int) {
 	checkAndEnlarge(data, location, 8)
 	f := any.(float64)
 	i := math.Float64bits(f)
-	(*data)[*location] = byte((i >> 56) & 0xff)
-	(*data)[*location+1] = byte((i >> 48) & 0xff)
-	(*data)[*location+2] = byte((i >> 40) & 0xff)
-	(*data)[*location+3] = byte((i >> 32) & 0xff)
-	(*data)[*location+4] = byte((i >> 24) & 0xff)
-	(*data)[*location+5] = byte((i >> 16) & 0xff)
-	(*data)[*location+6] = byte((i >> 8) & 0xff)
-	(*data)[*location+7] = byte((i) & 0xff)
+	loc := *location
+	binary.BigEndian.PutUint64((*data)[loc:loc+8], i)
 	*location += 8
 }
 
 func (this *Float64) get(data *[]byte, location *int) interface{} {
-	var result uint64
-	result = uint64(0xff&(*data)[*location])<<56 |
-		uint64(0xff&(*data)[*location+1])<<48 |
-		uint64(0xff&(*data)[*location+2])<<40 |
-		uint64(0xff&(*data)[*location+3])<<32 |
-		uint64(0xff&(*data)[*location+4])<<24 |
-		uint64(0xff&(*data)[*location+5])<<16 |
-		uint64(0xff&(*data)[*location+6])<<8 |
-		uint64(0xff&(*data)[*location+7])
+	loc := *location
+	result := binary.BigEndian.Uint64((*data)[loc : loc+8])
 	*location += 8
 	return math.Float64frombits(result)
 }
