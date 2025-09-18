553827478dc2ec7e305e3884dfd34ab49633b021
Sharon Aicler <saichler@gmail.com>
2025-09-18 04:47:05 -0700

    fic test

--- go/tests/TestInit.go
+++ go/tests/TestInit.go
@@ -3,6 +3,7 @@ package tests
 import (
 	. "github.com/saichler/l8test/go/infra/t_resources"
 	"github.com/saichler/l8types/go/ifs"
+	"github.com/saichler/l8types/go/types/l8sysconfig"
 	"github.com/saichler/l8utils/go/utils/registry"
 	"github.com/saichler/l8utils/go/utils/resources"
 )
@@ -10,7 +11,7 @@ import (
 var globals ifs.IResources
 
 func init() {
-	config := &types.SysConfig{MaxDataSize: resources.DEFAULT_MAX_DATA_SIZE,
+	config := &l8sysconfig.L8SysConfig{MaxDataSize: resources.DEFAULT_MAX_DATA_SIZE,
 		RxQueueSize: resources.DEFAULT_QUEUE_SIZE,
 		TxQueueSize: resources.DEFAULT_QUEUE_SIZE,
 		LocalAlias:  "tests"}
